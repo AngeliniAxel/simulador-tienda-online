50aca59cced3f5217eb39ac4c1cf5e7557b1aaa2
AngeliniAxel <axel.angelini@gmail.com>
2025-03-16 17:45:22 +0100

    Added cart container with functionality to hide it

--- js/main.js
+++ js/main.js
@@ -5,3 +5,11 @@ selectBrand.addEventListener('change', renderByBrand);
 
 const nameInput = document.querySelector('.search-bar');
 nameInput.addEventListener('input', renderByName);
+
+const toggleDisplay = (itemName) => {
+    const item = document.querySelector(itemName);
+    item.classList.toggle('hidden');
+};
+
+const cartBtn = document.querySelector('.cart-icon');
+cartBtn.addEventListener('click', () => toggleDisplay('.cart-container'));
